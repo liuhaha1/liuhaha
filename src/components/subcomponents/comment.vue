8a934abeeb3cba258a977a8c17e59ecb7086838f
liuhaha <1498387175@qq.com>
2019-02-26 15:41:41 +0800

    第三天项目

--- src/components/subcomponents/comment.vue
+++ src/components/subcomponents/comment.vue
@@ -2,9 +2,9 @@
   <div class="cmt-container">
     <h3>发表评论</h3>
     <hr>
-    <textarea placeholder="请输入要BB的内容（做多吐槽120字）" maxlength="120"></textarea>
+    <textarea placeholder="请输入要BB的内容（做多吐槽120字）" maxlength="120" v-model="msg"></textarea>
 
-    <mt-button type="primary" size="large">发表评论</mt-button>
+    <mt-button type="primary" size="large" @click="postComment">发表评论</mt-button>
 
     <div class="cmt-list">
       <div class="cmt-item" v-for="(item, i) in comments" :key="item.i">
@@ -27,9 +27,9 @@ import { Toast } from "mint-ui";
 export default {
   data() {
     return {
-      pageIndex: 1, 
-      comments: [], 
-      msg:""
+      pageIndex: 1, // 默认展示第一页数据
+      comments: [], // 所有的评论数据
+      msg: "" // 评论输入的内容
     };
   },
   created() {
@@ -37,10 +37,13 @@ export default {
   },
   methods: {
     getComments() {
+      // 获取评论
       this.$http
         .get("api/getcomments/" + this.id + "?pageindex=" + this.pageIndex)
         .then(result => {
           if (result.body.status === 0) {
+            // this.comments = result.body.message;
+            // 每当获取新评论数据的时候，不要把老数据清空覆盖，而是应该以老数据，拼接上新数据
             this.comments = this.comments.concat(result.body.message);
           } else {
             Toast("获取评论失败！");
@@ -48,8 +51,32 @@ export default {
         });
     },
     getMore() {
+      // 加载更多
       this.pageIndex++;
       this.getComments();
+    },
+    postComment() {
+      // 校验是否为空内容
+      if (this.msg.trim().length === 0) {
+        return Toast("评论内容不能为空！");
+      }
+
+      this.$http
+        .post("api/postcomment/" + this.$route.params.id, {
+          content: this.msg.trim()
+        })
+        .then(function(result) {
+          if (result.body.status === 0) {
+            // 1. 拼接出一个评论对象
+            var cmt = {
+              user_name: "匿名用户",
+              add_time: Date.now(),
+              content: this.msg.trim()
+            };
+            this.comments.unshift(cmt);
+            this.msg = "";
+          }
+        });
     }
   },
   props: ["id"]
--- src/main.js
+++ src/main.js
@@ -20,8 +20,8 @@ Vue.use(VueResource)
 
 // 设置请求的根路径
 Vue.http.options.root = 'http://www.liulongbin.top:3005';
-
-
+// 全局设置 post 时候表单数据格式组织形式   application/x-www-form-urlencoded
+Vue.http.options.emulateJSON = true;
 
 // 导入 MUI 的样式
 import './lib/mui/css/mui.min.css'
@@ -29,12 +29,32 @@ import './lib/mui/css/mui.min.css'
 import './lib/mui/css/icons-extra.css'
 
 
-// 按需导入 Mint-UI 中的组件 
-import { Header, Swipe, SwipeItem, Button } from 'mint-ui'
+// 按需导入 Mint-UI 中的组件   
+/* import { Header, Swipe, SwipeItem, Button, Lazyload } from 'mint-ui'
 Vue.component(Header.name, Header)
 Vue.component(Swipe.name, Swipe)
 Vue.component(SwipeItem.name, SwipeItem)
 Vue.component(Button.name, Button)
+Vue.use(Lazyload); */
+
+import MintUI from 'mint-ui'
+Vue.use(MintUI)
+import 'mint-ui/lib/style.css'
+
+// 安装 图片预览插件
+import VuePreview from 'vue-preview'
+Vue.use(VuePreview)
+//配置
+Vue.use(VuePreview, {
+  mainClass: 'pswp--minimal--dark',
+  barsSize: {top: 0, bottom: 0},
+  captionEl: false,
+  fullscreenEl: false,
+  shareEl: false,
+  bgOpacity: 0.85,
+  tapToClose: true,
+  tapToToggleControls: false
+})
 
 // 1.3 导入自己的 router.js 路由模块
 import router from './router.js'
